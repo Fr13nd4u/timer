e3e909e08248896291b7af7254cd43e429198334
Fr13nd4u <www.maxom0796@gmail.com>
2020-12-12 14:02:02 +0200

    commit1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import Display from './Components/Display';
+import Btn from './Components/Btn';
 import './App.css';
 
 function App() {
+  const [time, setTime] = useState({s:0, m:0, h:0});
+  const [interv, setInterv] = useState();
+  const [status, setStatus] = useState(0);
+  // Not started = 0
+  // started = 1
+  // stopped = 2
+
+  const start = () => {
+    run();
+    setStatus(1);
+    setInterv(setInterval(run, 10));
+  };
+
+  var updatedS = time.s, updatedM = time.m, updatedH = time.h;
+
+  const run = () => {
+    if(updatedM === 60){
+      updatedH++;
+      updatedM = 0;
+    }
+    if(updatedS === 60){
+      updatedM++;
+      updatedS = 0;
+    }
+    
+    updatedS++;
+    return setTime({s:updatedS, m:updatedM, h:updatedH});
+  };
+
+  const wait = () => {
+    clearInterval(interv);
+    setStatus(2);
+    setInterval(300);
+  };
+
+  const reset = () => {
+    clearInterval(interv);
+    setStatus(0);
+    setTime({ms:0, s:0, m:0, h:0})
+  };
+
+  const resume = () => start();
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main-section">
+     <div className="clock-holder">
+          <div className="timer">
+               <Display time={time}/>
+               <Btn status={status} resume={resume} reset={reset} wait={wait} start={start}/>
+          </div>
+     </div>
     </div>
   );
 }
---
+++ src/Components/Btn.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+function Btn(props) {
+  return (
+    <div>
+      {(props.status === 0)?
+        <button className="timer-btn timer-btn-gre"
+        onClick={props.start}>Start</button> : ""
+      }
+
+      {(props.status === 1)?
+        <div>
+          <button className="timer-btn timer-btn-red"
+            onDoubleClick={props.wait}>Wait</button>
+          <button className="timer-btn timer-btn-yel"
+            onClick={props.reset}>Reset</button>
+        </div> : ""
+      } 
+
+      {(props.status === 2)? 
+        <div>
+          <button className="timer-btn timer-btn-gre"
+            onClick={props.resume}>Start</button>
+          <button className="timer-btn timer-btn-yel"
+            onClick={props.reset}>Reset</button>
+        </div> : ""
+      }
+
+    </div>
+  );
+}
+
+export default Btn;
