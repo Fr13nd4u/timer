e04d0c50b3135a97a226641ad770b9a4cf952f50
Fr13nd4u <www.maxom0796@gmail.com>
2021-02-23 14:50:28 +0200

    com1

--- src/Components/Btn.js
+++ src/Components/Btn.js
@@ -10,9 +10,9 @@ function Btn(props) {
 
       {(props.status === 1)?
         <div>
-          <button className="timer-btn timer-btn-red"
-            onDoubleClick={props.wait}>Wait</button>
           <button className="timer-btn timer-btn-yel"
+            onDoubleClick={props.wait}>Wait</button>
+          <button className="timer-btn timer-btn-red"
             onClick={props.reset}>Reset</button>
         </div> : ""
       } 
